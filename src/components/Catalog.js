fc54158dd99a4865a5fe9b8780ee5a0429107dec
dido-r <deyan_r@abv.bg>
2023-03-09 03:28:37 +0200

    Catalog Filter

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,26 +1,58 @@
 import { useEffect, useState } from 'react';
-import { Categories } from './Categories';
 import { Product } from './Product';
 import styles from './style/Catalog.module.css';
 
 export function Catalog() {
 
-    const gender = "Female";  //just for test
+    const gender = "Male";  //just for test
     let bannerSrc = "";
-    gender === "Male" ? bannerSrc="https://res.cloudinary.com/diby8tbnn/image/upload/v1677854615/BANNER-MENS-CLOTHING_outope.jpg" : bannerSrc="https://res.cloudinary.com/diby8tbnn/image/upload/v1677854615/banner-home-alexinternational-WOMENS-CLOTHING_gmxjbn.jpg"
+    gender === "Male" ? bannerSrc = "https://res.cloudinary.com/diby8tbnn/image/upload/v1677854615/BANNER-MENS-CLOTHING_outope.jpg" : bannerSrc = "https://res.cloudinary.com/diby8tbnn/image/upload/v1677854615/banner-home-alexinternational-WOMENS-CLOTHING_gmxjbn.jpg"
 
-    const[products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [valuePrice, setValuePrice] = useState("all");
+    const [valueSize, setValueSize] = useState("all");
+    const [categoryType, setCategoryType] = useState("all");
+
+    const onChangeOfCategory = (e) => {
+        setCategoryType(e.target.value);
+    }
+
+    const onChangeOfPrice = (e) => {
+        setValuePrice(e.target.value);
+    }
+
+    const onChangeOfSize = (e) => {
+        setValueSize(e.target.value);
+    }
+
+    const onClearCategory = () => {
+        setCategoryType("all");
+    }
+
+    const onClearSize = () => {
+        setValueSize("all");
+    }
+
+    const onClearPrice = () => {
+        setValuePrice("all");
+    }
+
+    const onClearAll = () => {
+        setCategoryType("all");
+        setValueSize("all");
+        setValuePrice("all");
+    }
 
     useEffect(() => {
-        fetch('https://parseapi.back4app.com/classes/Products', 
-        {
-            headers: {
-                "X-Parse-Application-Id": "mWelAz1zpW0lQMPIwD8xQs7BUgy1YhWGy1Zt8wB1",
-                "X-Parse-REST-API-Key" : "iS3NuKzNfFCSnW8T1htlC4wvsgFm0vYgBbnrOTdU"
-            }
-        })
-        .then(x => x.json())
-        .then(x => setProducts(x.results))
+        fetch('https://parseapi.back4app.com/classes/Products',
+            {
+                headers: {
+                    "X-Parse-Application-Id": "mWelAz1zpW0lQMPIwD8xQs7BUgy1YhWGy1Zt8wB1",
+                    "X-Parse-REST-API-Key": "iS3NuKzNfFCSnW8T1htlC4wvsgFm0vYgBbnrOTdU"
+                }
+            })
+            .then(x => x.json())
+            .then(x => setProducts(x.results))
     }, []);
 
     return (
@@ -28,50 +60,70 @@ export function Catalog() {
             <div className={styles['collection-header']}>
                 <div className={styles['collection-hero']}>
                     <div className={styles['collection-hero-img-container']}><img className={styles['collection-hero-image']}
-                    src={bannerSrc} /></div>
+                        src={bannerSrc} /></div>
                 </div>
             </div>
+            
+            {categoryType !== "all" && <button className={styles['clear-filter-btn']} onClick={onClearCategory}>x {categoryType}</button>}
+            {valueSize !== "all" && <button className={styles['clear-filter-btn']} onClick={onClearSize}>x {valueSize}</button>}
+            {valuePrice !== "all" && <button className={styles['clear-filter-btn']} onClick={onClearPrice}>x {valuePrice}</button>}
+            {(valueSize !== "all" || valuePrice !== "all" || categoryType !== "all") && <button className={styles['clear-filter-btn']} onClick={onClearAll}>Clear All</button>}
 
-            <div className={styles['grid-container']}>
+            <div className={styles['flex-container']}>
 
                 <div className={styles['catalog-sidebar']}>
                     <div>
                         <h2 className={styles['sidebar-h2']}> Categories</h2>
                     </div>
-                    <div>
-                        <ul className={styles['sidebar-ul']}>
-                            <Categories products={products} gender={gender}/>
-                        </ul>
+                    <div className={styles['form-container']}>
+                        {
+                            gender === "Male" ?
+                                <form>
+                                    <div><label htmlFor="Jackets"><input id="Jackets" name="category" type="radio" checked={categoryType === "Jackets"} value="Jackets" onChange={onChangeOfCategory} />Jackets</label></div>
+                                    <div><label htmlFor="Hoodies"><input id="Hoodies" name="category" type="radio" checked={categoryType === "Hoodies"} value="Hoodies" onChange={onChangeOfCategory} />Hoodies</label></div>
+                                    <div><label htmlFor="Shirts"><input id="Shirts" name="category" type="radio" checked={categoryType === "Shirts"} value="Shirts" onChange={onChangeOfCategory} />Shirts</label></div>
+                                    <div><label htmlFor="T-shirts"><input id="T-shirts" name="category" type="radio" checked={categoryType === "T-shirts"} value="T-shirts" onChange={onChangeOfCategory} />T-shirts</label></div>
+                                    <div><label htmlFor="Jeans"><input id="Jeans" name="category" type="radio" checked={categoryType === "Jeans"} value="Jeans" onChange={onChangeOfCategory} />Jeans</label></div>
+
+                                </form>
+                                :
+                                <form>
+                                    <div><label htmlFor="Jackets"><input id="Jackets" name="category" type="radio" checked={categoryType === "Jackets"} value="Jackets" onChange={onChangeOfCategory} />Jackets</label></div>
+                                    <div><label htmlFor="Shirts"><input id="Shirts" name="category" type="radio" checked={categoryType === "Shirts"} value="Shirts" onChange={onChangeOfCategory} />Shirts</label></div>
+                                    <div><label htmlFor="Dresses"><input id="Dresses" name="category" type="radio" checked={categoryType === "Dresses"} value="Dresses" onChange={onChangeOfCategory} />Dresses</label></div>
+                                    <div><label htmlFor="Jeans"><input id="Jeans" name="category" type="radio" checked={categoryType === "Jeans"} value="Jeans" onChange={onChangeOfCategory} />Jeans</label></div>
+                                </form>
+                        }
                     </div>
                     <div>
                         <h2 className={styles['sidebar-h2']}>Size</h2>
                     </div>
-                    <div>
-                        <ul className={styles['sidebar-ul']}>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>XS</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>S</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>M</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>L</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>XL</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>XXL</a></li>
-                        </ul>
+                    <div className={styles['form-container']}>
+                        <form>
+                            <div><label htmlFor="XS"><input id="XS" name="price" type="radio" value={"XS"} checked={valueSize === "XS"} onChange={onChangeOfSize} />XS</label></div>
+                            <div><label htmlFor="S"><input id="S" name="price" type="radio" value={"S"} checked={valueSize === "S"} onChange={onChangeOfSize} />S</label></div>
+                            <div><label htmlFor="M"><input id="M" name="price" type="radio" value={"M"} checked={valueSize === "M"} onChange={onChangeOfSize} />M</label></div>
+                            <div><label htmlFor="L"><input id="L" name="price" type="radio" value={"L"} checked={valueSize === "L"} onChange={onChangeOfSize} />L</label></div>
+                            <div><label htmlFor="XL"><input id="XL" name="price" type="radio" value={"XL"} checked={valueSize === "XL"} onChange={onChangeOfSize} />XL</label></div>
+                            <div><label htmlFor="XXL"><input id="XXL" name="price" type="radio" value={"XXL"} checked={valueSize === "XXL"} onChange={onChangeOfSize} />XXL</label></div>
+                        </form>
                     </div>
                     <div>
                         <h2 className={styles['sidebar-h2']}>Price</h2>
                     </div>
-                    <div>
-                        <ul className={styles['sidebar-ul']}>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>under 100</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>101 - 200</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>201 - 300</a></li>
-                            <li className={styles['sidebar-li']}><a href="#;" className={styles['sidebar-a']}><input type={'checkbox'} disabled={true}/>over 300</a></li>
-                        </ul>
+                    <div className={styles['form-container']}>
+                        <form>
+                            <div><label htmlFor="option1"><input id="option1" name="price" type="radio" value={"Under 100"} checked={valuePrice === "Under 100"} onChange={onChangeOfPrice} />under 100</label></div>
+                            <div><label htmlFor="option2"><input id="option2" name="price" type="radio" value={"100 - 200"} checked={valuePrice === "100 - 200"} onChange={onChangeOfPrice} />100 - 200</label></div>
+                            <div><label htmlFor="option3"><input id="option3" name="price" type="radio" value={"200 - 300"} checked={valuePrice === "200 - 300"} onChange={onChangeOfPrice} />200 - 300</label></div>
+                            <div><label htmlFor="option4"><input id="option4" name="price" type="radio" value={"Over 300"} checked={valuePrice === "Over 300"} onChange={onChangeOfPrice} />over 300</label></div>
+                        </form>
                     </div>
                 </div>
 
-                <div className={styles['catalog-grid']}>
-                    <Product products={products} gender={gender}/>
-                </div>    
+                <div className={styles['catalog-flex']}>
+                    <Product products={products} gender={gender} valueSize={valueSize} valuePrice={valuePrice} categoryType={categoryType} />
+                </div>
             </div>
 
             <div className={styles['pagination']}>
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,12 +2,42 @@ import styles from './style/Catalog.module.css';
 
 export function Product({
     products,
-    gender
+    gender,
+    valueSize,
+    valuePrice,
+    categoryType
 }) {
-    return (
-        
-        products.filter(z => z.gender === gender).map(x => (
+
+    const filterList = (current) => {
+
+        if (valuePrice !== "all") {
+
+            if (valuePrice === "Under 100") {
+
+                return current.price < 100;
+
+            } else if (valuePrice === "100 - 200") {
+
+                return current.price > 100 && current.price <= 200;
+
+            } else if (valuePrice === "200 - 300") {
+
+                return current.price > 200 && current.price <= 300;
+
+            } else if (valuePrice === "Over 300") {
+
+                return current.price > 300;
+            }
+        } else {
             
+            return true;
+        }
+    }
+
+    return (
+
+        // products.filter(z => z.gender === gender).filter(z => categoryType !== "all" ? z.type === categoryType : z).filter(z => valueSize !== "all" ? z.size.includes(valueSize) : z).filter(x => filterList(x)).map(x => (
+            products.filter(x => x.gender === gender && (categoryType !== "all" ? x.type === categoryType : x) && (valueSize !== "all" ? x.size.includes(valueSize) : x) && filterList(x)).map(x => (
             <div key={x.objectId} className={styles['catalog-current']}>
                 <a href="#">
                     <img className={styles['product-image']} src={x.imgUrl} />
@@ -16,7 +46,10 @@ export function Product({
                     <a href="#" className={styles['catalog-product-name']}>{x.name}</a>
                 </div>
                 <div >
-                    <span className="price">${x.price}</span>
+                    <span>${x.price}</span>
+                </div>
+                <div className={styles['ava-size']}>
+                    Available sizes: {x.size.join(', ')}
                 </div>
             </div>
         ))
